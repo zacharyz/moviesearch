e5ea7bb5612cc47fb5cc09a4a483d02d67687431
Zachary Zadell <zacharyz@gmail.com>
2019-10-20 14:42:35 -0700

    updated react refactored search bar to use hooks

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,50 +1,42 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from 'react-redux';
 import { fetchMovies } from '../actions/index';
 
-class SearchBar extends Component {
-   constructor(props) {
-      super(props);
-      this.state = { searchQuery: ''};
-      this.onInputChange = this.onInputChange.bind(this);
-      this.onFormSubmit = this.onFormSubmit.bind(this);
-   }
+const SearchBar = (props) => {
+   const [query, setQuery] = React.useState('');
 
-   onInputChange(event) {
-      this.setState({ searchQuery: event.target.value})
-   }
-   onFormSubmit(event) {
-      event.preventDefault();
-
-      // fetch movies for that search query
-      this.props.fetchMovies(this.state.searchQuery);
-      // this.setState({searchQuery:''});
+   const onInputChange = event => {
+      setQuery(event.target.value);
    }
    
-   render() {
-      return (
-         <div className="ui menu">
-            <div className="header item">NAVIS Movie Search</div>
-            <div className="right menu">
-               <div className="item">
-                  <div className="ui action left icon input">
+   const onFormSubmit = event => {
+      event.preventDefault();
+      // fetch movies for that search query
+      props.fetchMovies(query);
+   }
+   
+   return (
+      <div className="ui menu">
+         <div className="header item">Movie Search</div>
+         <div className="right menu">
+            <div className="item">
+               <div className="ui action left icon input">
+                  <i className="search icon"></i>
+                  <form onSubmit={onFormSubmit} className="ui action left icon input">
                      <i className="search icon"></i>
-                     <form onSubmit={this.onFormSubmit} className="ui action left icon input">
-                        <i className="search icon"></i>
-                        <input
-                           placeholder="Search for movies"
-                           className="form-control"
-                           value={this.state.searchQuery}
-                           onChange={this.onInputChange}
-                        />
-                        <button className="ui button">Submit</button>
-                     </form>
-                  </div>
+                     <input
+                        placeholder="Search for movies"
+                        className="form-control"
+                        value={query}
+                        onChange={onInputChange}
+                     />
+                     <button className="ui button">Submit</button>
+                  </form>
                </div>
             </div>
          </div>
-      );
-   }
+      </div>
+   );
 }
 
 export default connect(
